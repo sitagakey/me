21baa0474e3fbb6ea8a08ee9d884242c4cc743dd
hikikomori <mebeusu@gmail.com>
2021-05-09 15:37:26 +0900

    add 20210509

--- src/lib/variables.ts
+++ src/lib/variables.ts
@@ -1,4 +1,11 @@
 /**
  * タグの一覧（必要に応じて増やす）
  */
-export const tags = ['日記', 'アプリ', 'Nuxt.js'];
+export const tags = [
+    '日記',
+    'アプリ',
+    'Nuxt.js',
+    'JavaScript',
+    'Next.js',
+    'TypeScript',
+];
