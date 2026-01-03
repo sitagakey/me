58b23a9411a436ff253184d397a14ac76d21f1de
sitagakey <25952997+sitagakey@users.noreply.github.com>
2026-01-04 00:15:22 +0900

    update aritcle description

--- src/lib/date/index.ts
+++ src/lib/date/index.ts
@@ -3,3 +3,7 @@ import { format } from 'date-fns';
 export const formatDate = (date: Date, formatStr: string = 'yyyy/MM/dd HH:mm'): string => {
   return format(date, formatStr);
 };
+
+export const formatDateForDescription = (date: Date, formatStr: string = 'yyyy年MM月dd日'): string => {
+  return format(date, formatStr);
+};
